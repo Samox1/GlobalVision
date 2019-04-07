00c45c555f47f94333ed4e46f5d43b253080bb7a
Szymon Baczynski <simon.baczynski@gmail.com>
2019-04-07 16:40:31 +0200

    kappa

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -5,19 +5,33 @@ import matplotlib
 import cv2
 import time
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture("wesele.mp4")
+check1, frame0 = video.read()
+gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
+frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+fps = video.get(cv2.CAP_PROP_FPS)
+width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+pxs = width * height
+print("Frames in video: " + str(int(frames)))
+print("Frames per second: " + str(int(fps)))
+print("All pixels in frame: "+str(int(width))+"x"+str(int(height))+" = " + str(int(pxs)))
+framecount = 1
 
 while True:
     check, frame = video.read()
-    #print(check)
-    #print(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    diff = cv2.subtract(gray0, gray)
+    pxnon0 = (cv2.countNonZero(diff) / pxs) * 100
+    gray0 = gray
+    framecount = framecount + 1
+    if pxnon0 > 40:
+        print(str(framecount) + ": " + str(pxnon0) + "%")
 
-    cv2.imshow("Capture", frame)
-    #cv2.waitKey(0)
+    cv2.imshow("Capture", diff)
+
     key = cv2.waitKey(1)
-
     if key == ord('q'):
-        time2 = time.time()
         break
 
 video.release()
